b97276afc4d80b2cc972bdeaf721db04d42ca6f9
PooyaLiebe <pooya.payvar26@gmail.com>
2025-02-19 13:19:06 +0000

    Modify

--- server/db.js
+++ server/db.js
@@ -4,11 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://pooyapayvar25:Po@6331@preventivewebapp.i7vs6.mongodb.net/?retryWrites=true&w=majority&appName=preventiveWebApp",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://pooyapayvar25:Po@6331@preventivewebapp.i7vs6.mongodb.net/?retryWrites=true&w=majority&appName=preventiveWebApp"
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
